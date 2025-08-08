31d65d8ec507b2dc91c9f89e077be21de5cdcbac
pokemon-101 <pokemonpudg@gmail.com>
2025-08-08 13:26:55 +0530

    limiting to node

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import app from './src/app';
-import Bun from 'bun';
+import app from './src/app.js';
+import { serve } from '@hono/node-server';
 
 const port = process.env.PORT || 3030;
 
-Bun.serve({
-  port: port,
+serve({
   fetch: app.fetch,
+  port: port,
 });
 
 console.log(`🚀 Server running on port ${port}`);
